70c1af2f8eebe5d3cc5fda604037c381b8402e9a
Nick P <nb.piscitello@gmail.com>
2015-09-21 00:25:07 -0400

    commented

--- src/testing/Test.java
+++ src/testing/Test.java
@@ -8,21 +8,29 @@ public class Test {
 	
 	public static void main(String args[])	{
 		
+			// login credentials
 		String host = "192.168.1.100";	int port = 222;
 		String user = "npiscitello_sftp";	String pass = "pass";
 		
 		try	{
+				// new session config object
 			JSch ssh = new JSch();
+				// new session object
 			Session session = ssh.getSession(user, host, port);
 			session.setPassword(pass);
 			session.setConfig("StrictHostKeyChecking", "no");
 			session.connect();
+				// open a comms channel
 	        Channel channel = session.openChannel("sftp");
 	        channel.connect();
 	        
+	        	// open a sftp-specific comms channel
 	        ChannelSftp sftp = (ChannelSftp) channel;
+	        	// store the list of files returned by ls
 	        Vector<ChannelSftp.LsEntry> files = sftp.ls("/home/*");
+	        	// get the first entry in the list of files
 	        ChannelSftp.LsEntry object = files.get(0);
+	        	// print the filename of the returned file
 	        System.out.println(object.getFilename());
 	        sftp.exit();
 	        channel.disconnect();
