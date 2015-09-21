9b82258962b7e0aae6d08053ee459e7fa3dea82f
Nick P <nb.piscitello@gmail.com>
2015-09-21 00:21:55 -0400

    Working barebones test demo

--- src/testing/Test.java
+++ src/testing/Test.java
@@ -1,4 +1,7 @@
 package testing;
+
+import java.util.Vector;
+
 import com.jcraft.jsch.*;
 
 public class Test {
@@ -8,5 +11,24 @@ public class Test {
 		String host = "192.168.1.100";	int port = 222;
 		String user = "npiscitello_sftp";	String pass = "pass";
 		
+		try	{
+			JSch ssh = new JSch();
+			Session session = ssh.getSession(user, host, port);
+			session.setPassword(pass);
+			session.setConfig("StrictHostKeyChecking", "no");
+			session.connect();
+	        Channel channel = session.openChannel("sftp");
+	        channel.connect();
+	        
+	        ChannelSftp sftp = (ChannelSftp) channel;
+	        Vector<ChannelSftp.LsEntry> files = sftp.ls("/home/*");
+	        ChannelSftp.LsEntry object = files.get(0);
+	        System.out.println(object.getFilename());
+	        sftp.exit();
+	        channel.disconnect();
+		}
+		catch(Exception e)	{
+			System.out.println(e);
+		}
 	}
 }
