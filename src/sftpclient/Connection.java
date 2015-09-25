f65f8a129eeb8a25260373c34175836a9aa3391e
Nick P <nb.piscitello@gmail.com>
2015-09-25 12:49:11 -0400

    Added status bar placeholder, continued developing settings tab, removed error in Connection

--- src/sftpclient/Connection.java
+++ src/sftpclient/Connection.java
@@ -3,4 +3,9 @@ package sftpclient;
 public class Connection {
 	
 	private String username, password;
+	
+	public void main()	{
+		System.out.println(username);
+		System.out.println(password);
+	}
 }
--- src/sftpclient/GUI.java
+++ src/sftpclient/GUI.java
@@ -6,6 +6,7 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 @SuppressWarnings("serial")
 public class GUI extends JFrame{
@@ -25,24 +26,37 @@ public class GUI extends JFrame{
 		dirViewer.setResizeWeight(0.5);
 		
 			// username/password labels
-		JLabel userlabel = new JLabel("username: ");
-		JLabel passlabel = new JLabel("password: ");
+		JLabel userlabel = new JLabel("username:");
+		JLabel passlabel = new JLabel("password:");
 		
 			// username/password entry windows
 		JTextField username = new JTextField(18);
 		JTextField password = new JTextField(18);
 		
+			// username/password enter buttons
+		Insets button_margins = new Insets(0,2,0,2);
+		JButton unenter = new JButton("enter"); unenter.setMargin(button_margins);
+		JButton pwenter = new JButton("enter");	pwenter.setMargin(button_margins);
+		
 			// pack the settings into the settings panel
 		GridBagConstraints constraints = new GridBagConstraints();
 		JPanel settings = new JPanel(new GridBagLayout());
-		constraints.gridx=0;	constraints.gridy=0;	constraints.anchor=GridBagConstraints.WEST;
-			constraints.fill=GridBagConstraints.BOTH;	constraints.weightx=0;	settings.add(userlabel, constraints);
-		constraints.gridx=1;	constraints.gridy=0;	constraints.weightx=1;
-			constraints.fill=GridBagConstraints.HORIZONTAL;	settings.add(username, constraints);
-		constraints.gridx=0;	constraints.gridy=1;	constraints.anchor=GridBagConstraints.WEST;
-			constraints.fill=GridBagConstraints.BOTH;	constraints.weightx=0;	settings.add(passlabel, constraints);
-		constraints.gridx=1;	constraints.gridy=1;	constraints.weightx=1;
-			constraints.fill=GridBagConstraints.HORIZONTAL;	settings.add(password, constraints);
+		Insets pad = new Insets(0,5,0,5);	Insets zero = new Insets(0,0,0,0);
+		constraints.gridx=0; constraints.gridy=0; constraints.weightx=0; constraints.weighty=0;
+			constraints.insets=pad; constraints.anchor=GridBagConstraints.WEST; settings.add(userlabel, constraints);
+		constraints.gridx=1; constraints.gridy=0; constraints.weightx=1; constraints.weighty=0;
+			constraints.insets=zero; constraints.fill=GridBagConstraints.HORIZONTAL; settings.add(username, constraints);
+		constraints.gridx=2; constraints.gridy=0; constraints.weightx=0; constraints.weighty=0;
+			constraints.insets=pad; constraints.anchor=GridBagConstraints.EAST; settings.add(unenter, constraints);
+		constraints.gridx=0; constraints.gridy=1; constraints.weightx=0; constraints.weighty=0;
+			constraints.insets=pad; constraints.anchor=GridBagConstraints.WEST; settings.add(passlabel, constraints);
+		constraints.gridx=1; constraints.gridy=1; constraints.weightx=1; constraints.weighty=0;
+			constraints.insets=zero; constraints.fill=GridBagConstraints.HORIZONTAL; settings.add(password, constraints);
+		constraints.gridx=2; constraints.gridy=1; constraints.weightx=0; constraints.weighty=0;
+			constraints.insets=pad; constraints.anchor=GridBagConstraints.EAST; settings.add(pwenter, constraints);
+			// dummy panel to take up all the extra space
+		constraints.gridx=0; constraints.gridy=2; constraints.weightx=1; constraints.weighty=1; constraints.insets=zero;
+			constraints.gridwidth=GridBagConstraints.REMAINDER; constraints.fill=GridBagConstraints.BOTH; settings.add(new JPanel(), constraints);
 		
 			// icons
 		ImageIcon folder = new ImageIcon(getClass().getResource("directory-icon.png"));
@@ -54,8 +68,12 @@ public class GUI extends JFrame{
 		tabbedPane.addTab("Directories", folder, dirViewer, "browse local and remote directories");
 		tabbedPane.addTab("Settings", gear, settings, "configure the application");
 		
-			// pack the tabbed pane into the window
+			// status bar
+		JLabel status_bar = new JLabel("status bar placeholder", JLabel.CENTER);
+		
+			// pack the tabbed pane and status bar into the window
 		getContentPane().add(tabbedPane, BorderLayout.CENTER);
+		getContentPane().add(status_bar, BorderLayout.SOUTH);
 		
 			// window properties
 		setTitle(name);
