dd7c25294721fac5d47c0bc5d16dc0b8ef3c9587
Nick P <nb.piscitello@gmail.com>
2015-09-28 16:34:26 -0400

    cleanup commit

--- src/sftpclient/GUI.java
+++ src/sftpclient/GUI.java
@@ -8,9 +8,11 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 @SuppressWarnings("serial")
-public class GUI extends JFrame{
+public class GUI extends JFrame implements ActionListener	{
 	
 		// resources
 	private ImageIcon folder = new ImageIcon(getClass().getResource("directory-icon.png"));
@@ -24,11 +26,21 @@ public class GUI extends JFrame{
 	private JTree remoteDir = new JTree();
 	private JTextField username = new JTextField(18);
 	private JTextField password = new JTextField(18);
+	private JTextField host = new JTextField(18);
+	private JTextField port = new JTextField(18);
 	private JButton credenter = new JButton(checkmark);
 	private JTextField default_dir = new JTextField(18);
 	private JButton browse = new JButton("Browse...");
+	private JLabel status = new JLabel("application launch successful", checkmark, JLabel.LEADING);
+	private JButton test = new JButton("Test");
 	
-	public GUI(String name, Dimension size, Dimension minsize) {		
+		// constructor - the class is instantiated in Backend and set visible by Backend.main()
+	public GUI(String name, Dimension size, Dimension minsize) {
+		
+			// set button action events
+		credenter.setActionCommand("credenter"); credenter.addActionListener(this);
+		browse.setActionCommand("browse"); browse.addActionListener(this);
+		test.setActionCommand("test"); test.addActionListener(this);
 		
 			// pack scrollable trees into a split pane; resize the sides dynamically
 		JSplitPane dirViewer = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, new JScrollPane(localDir), new JScrollPane(remoteDir));
@@ -41,11 +53,16 @@ public class GUI extends JFrame{
 		cred_const.gridx=0; cred_const.weightx=0; cred_const.insets=pad; cred_const.anchor=GridBagConstraints.WEST;
 			cred_const.gridy=0; credentials.add(new JLabel("username:"), cred_const);
 			cred_const.gridy=1; credentials.add(new JLabel("password:"), cred_const);
+			cred_const.gridy=2; credentials.add(new JLabel("host:"), cred_const);
+			cred_const.gridy=3; credentials.add(new JLabel("port:"), cred_const);
 		cred_const.gridx=1; cred_const.weightx=1; cred_const.insets=zero; cred_const.fill=GridBagConstraints.HORIZONTAL;
 			cred_const.gridy=0; credentials.add(username, cred_const);
 			cred_const.gridy=1; credentials.add(password, cred_const);
+			cred_const.gridy=2; credentials.add(host, cred_const);
+			cred_const.gridy=3; credentials.add(port, cred_const);
 		cred_const.gridx=2; cred_const.weightx=0; cred_const.insets=pad; cred_const.anchor=GridBagConstraints.EAST;
 			cred_const.gridy=0; cred_const.gridheight=2; credenter.setMargin(new Insets(0,2,0,2)); credentials.add(credenter, cred_const);
+			cred_const.gridy=2; cred_const.gridheight=2; credentials.add(test, cred_const);
 		
 			// pack the default directory selector into a panel
 		JPanel default_dir_sel = new JPanel(new GridBagLayout());
@@ -80,10 +97,8 @@ public class GUI extends JFrame{
 			// pack components into the status bar
 		JPanel status_bar = new JPanel(new GridBagLayout());
 		GridBagConstraints status_const = new GridBagConstraints();
-		status_const.gridx=0; status_bar.add(new JLabel(checkmark, JLabel.CENTER), status_const);
-		status_const.gridx=1; status_bar.add(new JLabel(xmark, JLabel.CENTER), status_const);
 		status_const.gridx=2; status_const.weightx=1; status_const.insets=pad; status_const.anchor=GridBagConstraints.WEST;
-			status_bar.add(new JLabel("status bar placeholder", JLabel.LEFT), status_const);
+			status_bar.add(status, status_const);
 		
 			// pack the tabbed pane and status bar into the window
 		getContentPane().add(tabbedPane, BorderLayout.CENTER);
@@ -94,10 +109,26 @@ public class GUI extends JFrame{
 		setSize(size);
 		setMinimumSize(minsize);
 		setIconImage(folder.getImage());
-		setVisible(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	
-	public static void main(String args[])	{
-		new GUI("SFTP Client", new Dimension(800,500), new Dimension(500,300));
+	public void updateStatus(boolean check, String message)	{
+		status.setText(message);
+		if(check)	{
+			status.setIcon(checkmark);
+		}	else	{
+			status.setIcon(xmark);
+		}
+	}
+	
+		// the contents of this method are almost exclusively for testing
+	public void actionPerformed(ActionEvent e)	{
+		if("credenter" == e.getActionCommand()){
+			Backend.updateCredentials(username.getText(), password.getText(), host.getText(), port.getText());
+		}	else if("browse" == e.getActionCommand()){
+			this.updateStatus(true, "browse button has been pressed");
+		}	else if("test" == e.getActionCommand()) {
+			Backend.connectionTest();
+		}
 	}
 }
